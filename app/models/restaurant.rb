506d74579802b2bde1bde4b470875f8a760599d3
Haruna Fujita <fujitaharuna.p@gmail.com>
2025-11-12 16:20:34 +0900

    finished barebones version of challenge 2

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,7 +2,7 @@ class Restaurant < ApplicationRecord
   # has_many :reviews, dependent: :destroy
   has_many :reviews, dependent: :destroy
 
-  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+  CATEGORIES = ["Chinese 🇨🇳", "Italian 🇮🇹", "Japanese 🇯🇵", "French 🇫🇷", "Belgian 🇧🇪"]
   validates :name, :address, :category, presence: true # no brackets
   validates :category, inclusion: { in: CATEGORIES }
 end
